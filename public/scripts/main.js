0c1fa501280872063a6518aed4f4ebd55c68b80e
andrenaught <andrenaught@gmail.com>
2018-03-20 15:24:14 -0700

    moving the website to public folder so we can use the dpd.js thing for user authentication

---
+++ public/scripts/main.js
@@ -0,0 +1,9 @@
+(function(window) {
+  "use strict";
+  var App = window.App;
+
+  //JS way of importing class definitions
+  //var user = App.User;
+dpd('users');
+
+})(window);
---
+++ public/scripts/user.js
@@ -0,0 +1,9 @@
+(function(window) {
+  "use strict";
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  console.log(dpd('users'));
+
+  window.App = App;
+})(window);
