55b9f543167436c05a2681444ca53c71b7dcc18a
choushruti88 <chou.shruti@gmail.com>
2018-04-03 21:44:26 -0700

    updating upload demand pages

--- public/scripts/inputCreator.js
+++ public/scripts/inputCreator.js
@@ -4,18 +4,19 @@
   var $ = window.jQuery;
 
   var demand_maker = "[data-form-demandlist=\"div\"]";
-  var num_demands = 0;
+  var num_demands = 1;
 
 
   var add_demand = function() {
-    var $demand_input = $("<span> demand " + num_demands + ": <input type='text' class='demand' required></span>");
+    var $demand_input = $("<div> Demand " + num_demands + ": <input type='text' class='demand' required></div>");
 
+    $demand_input.addClass("form-group");
 
     num_demands++;
 
     $(demand_maker).append($demand_input);
-    $(demand_maker).append("<br>");
-  }
+    //$(demand_maker).append("<br>");
+  };
 
 
   App.add_demand = add_demand;
